cd24089c8177f48ede3cebf477b94e74afb333f4
qingqingkang <isabellakqq@gmail.com>
2021-08-11 21:34:45 -0700

    Implemented SignUP, Login and Authentication and already tested by postman https://www.getpostman.com/collections/ef80329659db57cea468

--- src/main/java/com/laioffer/onlineOrder/controller/SignUpController.java
+++ src/main/java/com/laioffer/onlineOrder/controller/SignUpController.java
@@ -1,5 +1,8 @@
 package com.laioffer.onlineOrder.controller;
+
 import com.laioffer.onlineOrder.entity.Customer;
+import com.laioffer.onlineOrder.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,11 +12,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class SignUpController {
-    //Use the @RequestMapping annotation to define REST API, such as HTTP URL, method, etc
-    @RequestMapping(value = "/signUp", method = RequestMethod.POST)
+
+    @Autowired
+    private CustomerService customerService;
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
     public void signUp(@RequestBody Customer customer) {
-        //handle signup resquest
-
+        customerService.signUp(customer);
     }
 }
+
